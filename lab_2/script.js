4b63b86b2199962a03cae1d836d3a0ed51a5b563
Oleksandr Tsybulskyy <tsybulskyy.sasha@gmail.com>
2024-10-02 15:20:19 +0300

    lab 2

---
+++ lab_2/script.js
@@ -0,0 +1,22 @@
+(function() {
+    var names = ["Bill", "John", "Jen", "Jason", "Paul", "Frank", "Steven", "Larry", "Paula", "Laura", "Jim"];
+
+    for (var i = 0; i < names.length; i++) {
+        var firstLetter = names[i].charAt(0).toLowerCase();
+
+        if (firstLetter === 'j') {
+            speakGoodBye(names[i]);
+        } else {
+            speakHello(names[i]);
+        }
+    }
+
+    console.log("\n--- Додатковий функціонал: Селекція за унікальними літерами ---");
+    for (var j = 0; j < names.length; j++) {
+        var uniqueLetters = new Set(names[j].toLowerCase()).size;
+
+        if (uniqueLetters > 4) {
+            console.log("Ім'я " + names[j] + " містить більше чотирьох унікальних літер.");
+        }
+    }
+})();
